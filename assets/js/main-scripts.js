1a198b5ae96d8893c2642f17188168bba468dc54
Sriharsha <>
2019-09-23 17:43:58 +0530

    updated folder structure

---
+++ assets/js/main-scripts.js
@@ -0,0 +1,350 @@
+// plug it in, plug it in
+function load() {
+	var someData_notJSON = JSON.parse(data);
+	console.log("someData_notJSON", someData_notJSON[0].red);
+}
+
+function loadJSON(callback) {
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+	xobj.open('GET', '../../environment.json', true);
+	xobj.onreadystatechange = function () {
+		if (xobj.readyState == 4 && xobj.status == "200") {
+			callback(xobj.responseText);
+		}
+	};
+	xobj.send(null);
+}
+
+function loadEnvironment() {
+	loadJSON(function (response) {
+		var actual_JSON = JSON.parse(response);
+		sessionStorage.setItem("environment", response);
+	});
+}
+if (!sessionStorage.getItem("environment")) {
+	loadEnvironment();
+	var environment = JSON.parse(sessionStorage.getItem("environment"));
+} else {
+	var environment = JSON.parse(sessionStorage.getItem("environment"));
+}
+var loginStatus = sessionStorage.getItem("userData") !== null ? true : false;
+if (loginStatus === true) {
+	window.location.href = window.location.origin + '/app/dashboard/home.html';
+}
+(function ($) {
+
+	var paraCount = function () {
+
+		return {
+
+			options: {
+				itemClass: 'awesome',
+				appPath: window.location.origin,
+				baseUrl2: environment.baseUrl,
+				navPath: "",
+				apiUrl: environment.baseApiUrl,
+				callMe: function () {
+					console.log("ring, ring...");
+				}
+			},
+
+			// --------------------------------------------------------------------
+
+			init: function (options, element) {
+				this.options = $.extend(this.options, options);
+				this.elem = element;
+				this.attachEvents();
+			},
+
+			// --------------------------------------------------------------------
+
+			attachEvents: function () {
+				var self = this;
+
+				var urlArr = ['login', 'signup', 'forgotPassword', 'home', 'account', 'share'];
+				// $.each(urlArr, function (index, value) {
+				// 	if (window.location.href.indexOf(urlArr + '.html') <= -1) {
+				// 		if (window.location.href.indexOf(value) >= 0) {
+				// 			window.location = self.options.appPath +'app/auth/'+ value + '.html';
+				// 		}
+				// 	}
+				// });
+
+
+				$('.transition').on('focus', function () {
+					$(this).next('.invalid-feedback').remove();
+				});
+
+				/* Resume Signin functionlity */
+				$('#signinResume').on('click', function () {
+					self.loginResumeUser();
+				});
+
+				/* Resume Signup functionlity */
+				$('#signupResume').on('click', function () {
+					self.singnupResumeUser();
+				});
+
+				/* Reset Password functionality */
+				$('#resetPassBtn').on('click', function () {
+					self.resetPassword();
+				});
+
+				/*$('input').on('keypress', function(e) {
+					if (e.which == 32)
+						return false;
+				});*/
+
+				$('body').on('keydown', function (e) {
+					var key = e.which;
+					if (key == 13) {
+
+						if ($(this).attr('id') == 'login') self.loginResumeUser();
+						if ($(this).attr('id') == 'signupResume') self.singnupResumeUser();
+						if ($(this).attr('id') == 'forgotPassword') self.resetPassword();
+					}
+				});
+
+			},
+
+			loginResumeUser: function () {
+				var self = this;
+				let username = $('input[name="email"]').val();
+				let password = $('input[name="password"]').val();
+
+				$('.invalid-feedback').remove();
+				var validate = true;
+				if ($.trim(username).length <= 0) {
+					validate = false;
+					$('<div class="invalid-feedback">Email is required.</div>').insertAfter('input[name="email"]');
+				} else {
+					var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+					if (!emailReg.test(username)) {
+						validate = false;
+						$('<div class="invalid-feedback">Please enter valid email.</div>').insertAfter('input[name="email"]');
+					}
+				}
+				if ($.trim(password).length <= 0) {
+					validate = false;
+					$('<div class="invalid-feedback">Password is required.</div>').insertAfter('input[name="password"]');
+				}
+
+				if (validate) {
+					$('#signinResume').attr('disabled', true);
+					let signupData = {
+						"username": username,
+						"password": password
+					};
+
+					console.log(signupData);
+					var settings = {
+						"async": true,
+						"crossDomain": true,
+						"url": environment.baseApiUrl + environment.serviceUrls.post.loginResumeUser,
+						"method": "POST",
+						"headers": {
+							'Access-Control-Allow-Origin': environment.baseApiUrl,
+							"token": environment.authToken,
+							"content-type": "application/json"
+						},
+						"processData": false,
+						"data": JSON.stringify(signupData)
+					}
+					$.ajax(settings).done(function (response) {
+						console.log(response);
+						response = typeof (response) !== "object" ? JSON.parse(response) : response;
+						sessionStorage.clear();
+						if (response.status == "success") {
+							sessionStorage.setItem('sessionId', response.sessionId);
+							sessionStorage.setItem("templateId", 2);
+							sessionStorage.setItem('userData', JSON.stringify(response.data));
+							sessionStorage.setItem('fromPage', 'signin');
+							sessionStorage.setItem('shareName', response.data.share_name);
+							sessionStorage.setItem('imageStore', response.data.pic);
+							window.location.href = window.location.origin + "/app/dashboard/home.html";
+						} else {
+							$('#newErrorMessageID').html(response.msg.description)
+								.fadeIn()
+								.delay(3000)
+								.fadeOut('slow', function () {
+									$('#signinResume').attr('disabled', false);
+								});
+						}
+					});
+				}
+			},
+
+			singnupResumeUser: function () {
+				var self = this;
+				let firstname = $.trim($('input[name="firstname"]').val());
+				let lastname = $.trim($('input[name="lastname"]').val());
+				let email = $.trim($('input[name="email"]').val());
+				let password = $.trim($('input[name="password"]').val());
+
+				$('.invalid-feedback').remove();
+
+				var validate = true;
+				if ($.trim(firstname).length <= 0) {
+					validate = false;
+					$('<div class="invalid-feedback">First Name is required.</div>').insertAfter('input[name="firstname"]');
+				} else {
+					if ($.trim(firstname).length < 3) {
+						validate = false;
+						$('<div class="invalid-feedback">Minimum 3 characters are required.</div>').insertAfter('input[name="firstname"]');
+					}
+				}
+
+				if ($.trim(lastname).length <= 0) {
+					validate = false;
+					$('<div class="invalid-feedback">Last Name is required.</div>').insertAfter('input[name="lastname"]');
+				} else {
+					if ($.trim(lastname).length < 3) {
+						validate = false;
+						$('<div class="invalid-feedback">Minimum 3 characters are required.</div>').insertAfter('input[name="lastname"]');
+					}
+				}
+				if ($.trim(email).length <= 0) {
+					validate = false;
+					$('<div class="invalid-feedback">Email is required.</div>').insertAfter('input[name="email"]');
+				} else {
+					var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+					if (!emailReg.test(email)) {
+						validate = false;
+						$('<div class="invalid-feedback">Please enter valid email.</div>').insertAfter('input[name="email"]');
+					}
+				}
+				if ($.trim(password).length <= 0) {
+					validate = false;
+					$('<div class="invalid-feedback">Password is required.</div>').insertAfter('input[name="password"]');
+				} else if ($.trim(password).length < 8) {
+					validate = false;
+					$('<div class="invalid-feedback">Password must be 8 characters. </div>').insertAfter('input[name="password"]');
+				}
+
+				if (validate) {
+
+					$('#signupResume').attr('disabled', true);
+
+					let signupData = {
+						"firstname": firstname,
+						"lastname": lastname,
+						"email": email,
+						"password": password
+					};
+
+					//console.log(signupData);
+					var settings = {
+						"async": true,
+						"crossDomain": true,
+						"url": environment.baseApiUrl + environment.serviceUrls.post.signupResume,
+						"method": "POST",
+						"headers": {
+							"token": environment.authToken,
+							"content-type": "application/json"
+						},
+						"processData": false,
+						"data": JSON.stringify(signupData)
+					}
+
+					$.ajax(settings).done(function (response) {
+						console.log('response new', response);
+						sessionStorage.clear();
+						if (response.status == "success") {
+							sessionStorage.setItem('userStatus', response.data.sessionId);
+							sessionStorage.setItem('sessionId', response.data.sessionId);
+							sessionStorage.setItem('userData', JSON.stringify(response.data));
+							sessionStorage.setItem('shareName', response.shareName);
+							sessionStorage.setItem('fromPage', 'signup');
+							sessionStorage.setItem('imageStore', '');
+							window.location.href = "../home/index.html";
+						} else {
+							$('#newErrorMessageID').html(response.msg.description)
+								.fadeIn()
+								.delay(3000)
+								.fadeOut('slow', function () {
+									$('#signupResume').attr('disabled', false);
+								});
+						}
+
+					});
+				}
+			},
+
+			resetPassword: function () {
+				$('.invalid-feedback').remove();
+				var self = this;
+				var email = $('input[name="email"]').val();
+				var validate = true;
+
+				if ($.trim(email).length <= 0) {
+					validate = false;
+					$('<div class="invalid-feedback">Email is required.</div>').insertAfter('input[name="email"]');
+				} else {
+					var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+					if (!emailReg.test(email)) {
+						validate = false;
+						$('<div class="invalid-feedback">Please enter valid email.</div>').insertAfter('input[name="email"]');
+					}
+				}
+
+				if (validate) {
+					$('#resetPassBtn').attr('disabled', true);
+					let userData = JSON.stringify({
+						'username': email
+					});
+					var settings = {
+						"async": true,
+						"crossDomain": true,
+						"url": environment.baseApiUrl + environment.serviceUrls.post.resetPasswordLinkToEmailResume,
+						"method": "POST",
+						"headers": {
+							"token": environment.authToken,
+							"content-type": "application/json",
+							"cache-control": "no-cache",
+						},
+						"processData": false,
+						"data": userData
+					}
+
+					$.ajax(settings).done(function (response) {
+						console.log(response);
+						if (response.status == "success") {
+							$('#newSuccessMessageID .message-text').html(response.msg.description);
+							$('#newSuccessMessageID').html(response.msg.description)
+								.fadeIn()
+								.delay(3000)
+								.fadeOut();
+						} else {
+							$('#newErrorMessageID .message-text').html(response.msg.description)
+							$('#newErrorMessageID').html(response.msg.description)
+								.fadeIn()
+								.delay(3000)
+								.fadeOut('slow', function () {
+									$('#resetPassBtn').attr('disabled', false);
+								});
+						}
+					});
+				}
+			},
+
+		};
+
+	};
+
+	// --------------------------------------------------------------------
+
+	$.fn.paraCount = function (options) {
+		// create an instance for each element in the set
+		console.log(options);
+		// return;
+		return this.each(function () {
+			var myParaCount = new paraCount();
+			myParaCount.init(options, this);
+			console.log(this);
+			// this lets us call methods on the selector
+			$(this).data("paraCount", myParaCount);
+		});
+	};
+
+})(jQuery);
---
+++ assets/js/main.js
@@ -0,0 +1,153 @@
+// Function to 'load JSON' data
+function load() {
+  var someData_notJSON = JSON.parse(data);
+  console.log("someData_notJSON", someData_notJSON[0].red);
+}
+
+function loadJSON(callback) {
+  var xobj = new XMLHttpRequest();
+  xobj.overrideMimeType("application/json");
+  xobj.open('GET', '../../environment.json', true);
+  xobj.onreadystatechange = function () {
+    if (xobj.readyState == 4 && xobj.status == "200") {
+      callback(xobj.responseText);
+    }
+  };
+  xobj.send(null);
+}
+
+function loadEnvironment() {
+  loadJSON(function (response) {
+    var actual_JSON = JSON.parse(response);
+    sessionStorage.setItem("environment", response);
+  });
+}
+loadEnvironment();
+
+
+// Mobile Navigation
+function togNav() {
+  var nav = document.getElementById("sidenav");
+  if (nav.style.width == '100vw') {
+    nav.style.width = '0';
+    nav.style.opacity = 0;
+    $("#header").removeClass("-opened");
+  } else {
+    nav.style.width = "100vw";
+    nav.style.opacity = 1;
+    $("#header").addClass("-opened");
+  }
+}
+
+
+//Show Menu on Scroll
+var topBlock = $("#topBlock").offset().top;
+$(window).scroll(function () {
+  if (($(window).scrollTop() > topBlock)) {
+    $("#header").addClass("on-scroll");
+    $("header.on-scroll .resume-logo").attr('src', 'assets/images/workruit-resume-logo-color.svg')
+  } else {
+    $("#header").removeClass("on-scroll");
+    $("header .resume-logo").attr('src', 'assets/images/workruit-resume-logo-white.svg')
+  }
+});
+
+// footer link active
+$(function () {
+  $('.footer-nav li a[href^="/' + window.location.pathname.split("/")[1] + '"]').addClass('footer__active');
+});
+// autoplay vide on anroid devices
+var video = document.getElementById('video');
+video.addEventListener('click', function () {
+  video.play();
+}, false);
+// animation effect
+var botOfTheF1 = $(".feature-1").offset().top + $(".feature-1").outerHeight(true);
+var botOfTheF2 = $(".feature-2").offset().top + $(".feature-2").outerHeight(true);
+var botOfTheF3 = $(".feature-3").offset().top + $(".feature-3").outerHeight(true);
+var botOfTheF4 = $(".feature-4").offset().top + $(".feature-4").outerHeight(true);
+
+var topOfTheF1 = $(".feature-1").offset().top;
+var topOfTheF2 = $(".feature-2").offset().top;
+var topOfTheF3 = $(".feature-3").offset().top;
+var topOfTheF4 = $(".feature-4").offset().top;
+
+
+if ($(window).width() > 1024) {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > (topOfTheF4 - 35)) {
+      $(".device__phone , .device__browser").addClass("stucked");
+    } else {
+      $(".device__phone, .device__browser").removeClass("stucked");
+    }
+  });
+} else if ($(window).width() > 768) {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > (topOfTheF4)) {
+      $(".device__phone, .device__browser").addClass("stucked");
+    } else {
+      $(".device__phone").removeClass("stucked");
+    }
+  });
+} else {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > (topOfTheF4 + 40)) {
+      $(".device__phone, .device__browser").addClass("stucked");
+    } else {
+      $(".device__phone, .device__browser").removeClass("stucked");
+    }
+  });
+}
+
+if ($(window).height() < 650) {
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > (topOfTheF4 + 1)) {
+      $(".device__phon, .device__browsere").addClass("stucked");
+    } else {
+      $(".device__phone, .device__browser").removeClass("stucked");
+    }
+  });
+}
+
+
+
+$(window).scroll(function () {
+  if (($(window).scrollTop() > (botOfTheF1 - 275)) && ($(window).scrollTop() < (topOfTheF3 - 275))) {
+    $("#phone-in-2, #browser-in-2").addClass("active");
+    $("#phone-in-2, #browser-in-2").removeClass("past");
+    $("#phone-in-1, #browser-in-1").removeClass("active");
+    $("#phone-in-1, #browser-in-1").addClass("past");
+    $("#phone-in-3, #browser-in-3").removeClass("active");
+    $("#phone-in-3, #browser-in-3").removeClass("past");
+    $("#phone-in-4, #browser-in-4").removeClass("active");
+    $("#phone-in-4, #browser-in-4").removeClass("past");
+  } else if (($(window).scrollTop() > (botOfTheF2 - 275)) && ($(window).scrollTop() < (topOfTheF4 -
+      275))) {
+    $("#phone-in-3, #browser-in-3").addClass("active");
+    $("#phone-in-3, #browser-in-3").removeClass("past");
+    $("#phone-in-1, #browser-in-1").removeClass("active");
+    $("#phone-in-1, #browser-in-1").removeClass("past");
+    $("#phone-in-2, #browser-in-2").removeClass("active");
+    $("#phone-in-2, #browser-in-2").addClass("past");
+    $("#phone-in-4, #browser-in-4").removeClass("active");
+    $("#phone-in-4, #browser-in-4").removeClass("past");
+  } else if ($(window).scrollTop() > (botOfTheF3 - 275)) {
+    $("#phone-in-4, #browser-in-4").addClass("active");
+    $("#phone-in-4, #browser-in-4").removeClass("past");
+    $("#phone-in-1, #browser-in-1").removeClass("active");
+    $("#phone-in-1, #browser-in-1").removeClass("past");
+    $("#phone-in-2, #browser-in-2").removeClass("active");
+    //$("#phone-in-2").removeClass("past");
+    $("#phone-in-3, #browser-in-3").removeClass("active");
+    $("#phone-in-3, #browser-in-3").addClass("past");
+  } else {
+    $("#phone-in-1, #browser-in-1").addClass("active");
+    $("#phone-in-1, #browser-in-1").removeClass("past");
+    $("#phone-in-2, #browser-in-2").removeClass("active");
+    $("#phone-in-2, #browser-in-2").removeClass("past");
+    $("#phone-in-3, #browser-in-3").removeClass("active");
+    $("#phone-in-3, #browser-in-3").removeClass("past");
+    $("#phone-in-4, #browser-in-4").removeClass("active");
+    $("#phone-in-4, #browser-in-4").removeClass("past");
+  }
+});
